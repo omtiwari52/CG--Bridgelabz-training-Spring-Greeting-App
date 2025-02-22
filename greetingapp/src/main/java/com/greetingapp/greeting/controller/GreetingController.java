9ac2148a1184ea07a47e3fe56ff4aaa314e3b883
omtiwari52 <tiwariom900@gmail.com>
2025-02-22 12:21:57 +0530

    [Om Tiwari] add: Greeting app initialized with different greeting controller methods and return JSON added✨

---
+++ greetingapp/src/main/java/com/greetingapp/greeting/controller/GreetingController.java
@@ -0,0 +1,32 @@
+package com.greetingapp.greeting.controller;
+
+
+import com.greetingapp.greeting.GreetingApplication;
+import com.greetingapp.greeting.dto.Greeting;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping   //returns a simple greeting
+    public Greeting getGreeting(){
+        return new Greeting("Hello from BridgeLabz!!");
+    }
+
+    @PostMapping("/custom")
+    public Greeting postGreeting(@RequestBody Greeting greeting){
+        return new Greeting("Hello " + greeting.getMessage() + " from BridgeLabz!!");
+    }
+
+    @PutMapping("/update")
+    public Greeting updateGreeting(@RequestBody Greeting greeting){
+        return new Greeting("Message updated: "+greeting.getMessage());
+    }
+
+
+    @DeleteMapping("/delete")
+    public Greeting deleteGreeting(){
+        return new Greeting("Message deleted successfully");
+    }
+}
